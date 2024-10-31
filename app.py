12f2e21eab7d3068957e083a9017e94b6b57edd5
rradeba <rradeba@gmail.com>
2024-10-31 12:06:27 -0400

    c1

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
-from web_socket_server import WebSocketServer, socketio
-from flask import render_template
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, send
 
-app = WebSocketServer().create_app()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
 
-message_storage = {}
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client Connected')
-
-@socketio.on('disconnect')  
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('message')
-def handle_message(data):
-    author = data.get('author')  
-    message = data.get('message')
-
-    if author and message:
-        print(f'New message from {author}: {message}')
-        
-        if author not in message_storage:
-            message_storage[author] = []
-        message_storage[author].append(message) 
-
-        socketio.emit('message', data)
-
-@socketio.on('get_user_messages')
-def handle_get_user_messages(author):
-    user_messages = message_storage.get(author, [])
-    socketio.emit('get_user_messages', {'author': author, 'messages': user_messages})
+message_id_counter = 0
 
 @app.route('/')
 def index():
     return render_template('WebSocketClient.html')
 
-if __name__ == '__main__': 
+
+@socketio.on('join')
+def handle_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send({'username': 'System', 'message': f'{username} has joined the room.'}, to=room)
+
+
+@socketio.on('message')
+def handle_message(data):
+    global message_id_counter
+    username = data['username']
+    message = data['message']
+    room = data.get('room')
+    
+    message_id_counter += 1  
+    send({'id': message_id_counter, 'username': username, 'message': message}, to=room)
+
+
+@socketio.on('edit_message')
+def handle_edit_message(data):
+    room = data['room']
+    send({'id': data['id'], 'username': data['username'], 'message': data['message']}, to=room)
+
+
+@socketio.on('delete_message')
+def handle_delete_message(data):
+    room = data['room']
+   
+    send({'id': data['id']}, to=room)
+
+if __name__ == '__main__':
     socketio.run(app, debug=True)
